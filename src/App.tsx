f124204524cd4642f917896a1c0d2bfa79117b93
HaiderGhauri <haideralikhanh70@gmail.com>
2025-03-27 17:24:21 +0500

    initialize project

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import About from "./components/layout/About";
+import Contact from "./components/layout/Conatct";
+import Home from "./components/layout/Hero";
+import Projects from "./components/layout/Projects";
+import Skills from "./components/layout/Skills";
+import Header from "./components/ui/Header";
+
+function App() {
+  return (
+    <div className="text-slate-100">
+      <div className="fixed w-full z-10 top-4 px-6 xl:px-0">
+      <Header />
+      </div>
+      <div className="max-w-6xl mx-auto top-2 relative px-6 xl:px-0">
+        <Home />
+        <About />
+        <Skills />
+        <Projects />
+        <Contact />
+      </div>
+    </div>
+  );
+}
+
+export default App;
