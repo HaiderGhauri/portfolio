4a2e7cdb4fdf9cec5018ed2e6384dc3622f8a6e7
HaiderGhauri <haideralikhanh70@gmail.com>
2025-03-28 13:33:58 +0500

    update title & header

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -6,7 +6,7 @@ const Header = () => {
     return ( 
         <div className="h-[60px] max-w-5xl mx-auto grid grid-cols-2 items-center justify-items-between px-6 md:px-8 rounded-full shadow-2xl bg-slate-900/95 bg-blend-darken border border-slate-700">
             <div>
-                <h1 className="text-2xl font-bold">Logo</h1>
+                <h1 className="text-xl font-bold">HaiderGhauri</h1>
             </div>
 
             <div className="flex justify-end">
